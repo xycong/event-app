d04f8afb05ad7dd7547cbf019a973de7424c934e
Victor Cong <xycong@uwaterloo.ca>
2017-09-06 13:45:25 -0400

    add JSON web tokens

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -17,6 +17,9 @@ config :api, Api.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+config :guardian, Guardian,
+  secret_key: "hMTaFT8gvzcovVtJDS8ZBi5Tzyh/NvzOuqYRRoBWOII0zRojkuY+JNKLrn5hbDtS"
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
